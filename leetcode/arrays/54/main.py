34ce68932a29f3f10846e0907571b52595078c01
Winiicius <winiciusalexandre@gmail.com>
2025-10-25 15:41:47 -0300

    feat (54): Adiciona descrição e solução do problema Spiral Matrix do LeetCode

---
+++ leetcode/arrays/54/main.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
+
+        while top <= bottom and left <= right:
+            # esquerda → direita
+            for i in range(left, right + 1):
+                res.append(matrix[top][i])
+            top += 1
+
+            # cima → baixo
+            for i in range(top, bottom + 1):
+                res.append(matrix[i][right])
+            right -= 1
+
+            # direita → esquerda
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    res.append(matrix[bottom][i])
+                bottom -= 1
+
+            # baixo → cima
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    res.append(matrix[i][left])
+                left += 1
+
+        return res
